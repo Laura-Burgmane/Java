ac09ace24db1a5362b46a10f320a1e77132866bc
Laura-Burgmane <165931715+Laura-Burgmane@users.noreply.github.com>
2024-04-03 17:19:28 +0100

    Create main.java

---
+++ main.java
@@ -0,0 +1,15 @@
+String season = "spring";
+
+    if (season == "winter") {
+      System.out.println("Wear a warm jacket!");
+    }
+    else if (season == "spring") {
+      System.out.println("Wear a T-shirt!");
+    }
+    else if (season == "summer") {
+      System.out.println("Wear a swimming suite!");
+    }
+    else if (season == "autumn") {
+      System.out.println("Wear a rain coat!");
+    }    
+  }
